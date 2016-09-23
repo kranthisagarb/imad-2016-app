e95a87fc556d2018df76ef4b8568b10c5849cbb6
kranthisagarb <kranthisagarb@gmail.com>
2016-09-23 03:15:48 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one'){
+    res.send('Article one requested and will be served')
+}
+
+app.get('/article-two'){
+    res.send('Article two requested and will be served')
+}
+
+app.get('/article-three'){
+    res.send('Article three requested and will be served')
+}
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
