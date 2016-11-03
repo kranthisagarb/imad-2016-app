4536868a266fbca898f9ab9e3d54cfdc4c088406
kranthisagarb <kranthisagarb@gmail.com>
2016-11-03 18:38:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 
 var config = {
-    user: 'coco98',
+    user: 'kranthisagarb',
     database: 'kranthisagarb',
     host:'db.imad.hasura-app.io',
     port: '5432',
@@ -19,8 +19,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
+  pool.query("SELECT * FROM test", function(err, result){
+      if(err){
+          result.status(500).send(err.toString());
+      } else{
+          res.send(JSON.stringify(result));
+      }
+  }
   
+  );
 });
 
 app.get('/article-one',function(req,res) {
